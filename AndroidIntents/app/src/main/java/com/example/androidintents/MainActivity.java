c9177b298b3034db677d01fb4f81e14a980beb61
Mafruha Nuhary <mafruha.nuhary@tuni.fi>
2021-01-09 07:32:47 +0200

    m

---
+++ AndroidIntents/app/src/main/java/com/example/androidintents/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.androidintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button_web,button_dial;
+
+    EditText text_data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button_web = findViewById(R.id.button_web);
+        text_data=findViewById(R.id.TextData);
+
+        button_dial = findViewById(R.id.button_dial);
+
+        button_web.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebPage(text_data.getText().toString());
+
+            }
+        });
+
+        button_dial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialPhoneNumber(text_data.getText().toString());
+            }
+        });
+    }
+
+    private void openWebPage(String url){
+        if(!url.startsWith("http://") || !url.startsWith("https://"))
+            url = "http://" + url;
+
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+    public void dialPhoneNumber(String phoneNumber) {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + phoneNumber));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+}
